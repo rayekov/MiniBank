ea9fd780c36d049cc7f38685c2cdac8928fcef70
rayekov <hervefraye@gmail.com>
2022-05-07 00:33:25 +0100

    added account number, Transaction class and deposit/withdraw BankAccount class methods with exceptions

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,22 +4,55 @@
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+
+                }
+                return balance;
+
+
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
 
         public BankAccount(string name, decimal initialBalance)
         {
             Owner = name;
-            Balance = initialBalance;   
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now,"Initial Balance");
 
         }
         public void MakeDeposit(decimal amount, DateTime date, string note )
         {
-
+            if (amount<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount),"Amount of Deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);  
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithDrawal(decimal amount, DateTime date, string note)
         {
-
+            if (amount<=0)
+            {
+                throw  new ArgumentOutOfRangeException(nameof(amount),"Amount of Withdrawal must be positive");
+            }
+            if (Balance - amount <0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this operation");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@ using MiniBank;
 
 var account = new BankAccount ("Franck", 10000) ;
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with an initial deposit of {account.Balance} XAF ...");
+// let's make a deposit of 15000
+account.MakeDeposit(15000, DateTime.Now, "Savings");
+Console.WriteLine(account.Balance);
+
+// let's make a withdrawal of 5000
+account.MakeWithDrawal(5000, DateTime.Now, "Bills");
+Console.WriteLine(account.Balance);
