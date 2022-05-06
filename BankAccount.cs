f3c1d8e13c53d1ff5398e58d5277ee177c0364f0
rayekov <hervefraye@gmail.com>
2022-05-06 01:21:41 +0100

    created bank account class and defined its properties

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace MiniBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Owner = name;
+            Balance = initialBalance;   
+
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note )
+        {
+
+        }
+
+        public void MakeWithDrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
 ﻿
+using MiniBank;
+
+var account = new BankAccount ("Franck", 10000) ;
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with an initial deposit of {account.Balance} XAF ...");
